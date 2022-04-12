04e01e3d023cd7f774eca8de3e6e219f757af4c8
ammicco1 <ngrecchi1@gmail.com>
2022-04-12 08:45:12 +0200

    run color

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
-echo "\n\n\t!! REMEMBER TO KILL THE PROCESS !!\n\n\tHAVE FUN!!\n\n"
+#!/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+VIOLET=$(tput setaf 5)
+NORMAL=$(tput sgr0)
+BLUE=$(tput setaf 6)
+
+echo "${VIOLET}  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+printf "\n  author: AMMICCO (https://github.com/ammicco1)\n\n"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+printf "${BO}\n    THE LOG FILE WILL BE CREATED IN THIS\n"
+printf "    DIRECTORY, EDIT THE keylogger.c FILE TO\n" 
+printf "    CHANGE THE PATH OF THE LOG FILE\n"
+
+printf "\n\n\t${RED}!! REMEMBER TO KILL THE PROCESS !!\n\n\t\t${GREEN} !! HAVE FUN !!${NORMAL}\n\n"
 
 make
 
